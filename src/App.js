6b1670927bc15e7a667cbc7ef5fe2c39bc8f93ed
Guilherme S <69318250+GlermS@users.noreply.github.com>
2021-03-24 19:09:02 -0300

    cleaning template and studying project component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
+import projectImage from './assets/project-images/example-image.PNG'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className = "page-title">Meu portifólio</h1>
       </header>
+      <div className = 'App-body'>
+        <div className= "project-card">
+          <div className= "project-card-content">
+            <div className= "project-image">
+              <img src = {projectImage}></img>
+            </div>
+            <div className="project-description-container">
+              <h2>Meu título</h2>
+              <div className="project-description">
+                <span>Descrição</span>
+                <p>Pnásod amsodma moamoa amsds asd oapdnpa asd  f ff asdfmoewewf  </p>
+              </div>
+              <p><span>Linguagens:</span> Javascript</p>
+              <p><span>Tecnologias:</span> React</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className = "App-footer">
+
+      </footer>
     </div>
   );
 }
